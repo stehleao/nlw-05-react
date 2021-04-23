74fdb82211e83877a2e69af120c67b5ba0447a45
Stephanie <stephanieleaof@gmail.com>
2021-04-23 11:08:04 -0300

    SSG

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -16,7 +16,7 @@ export default function Player() {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-          <div className={styles.emptySlider} />
+            <div className={styles.emptySlider} />
           </div>
           <span>00:00</span>
         </div>
